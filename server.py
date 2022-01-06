c86086ad969869f90854badf7dd6906be0abdd23
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-01-06 14:01:54 -0500

    Correct Upload of Levels 2 and 3

--- server.py
+++ server.py
@@ -6,7 +6,12 @@ def Helloworld():
 @app.route('/play')
 def play():
     return render_template('index.html')
-
+@app.route('/play/<int:num>')
+def playnumber(num):
+    return render_template('index2.html',num=num)
+@app.route('/play/<int:num>/<color>')
+def playnunmbercolor(num,color):
+    return render_template('index3.html', num=num, color=color)
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
 
